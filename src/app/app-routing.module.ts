f9294db778eea66b49a9a4844ff2fb7552833101
Ábel Énekes <enekes.abel@gmail.com>
2019-06-18 19:51:39 +0200

    home module created, added to routes. app component updated to display router-outlet in drawer content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomeModule'
+  },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
